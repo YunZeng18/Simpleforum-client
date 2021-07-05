2ba1b05fa1325d47803d9ad8bff97916f445b867
7yundao <yunzeng18@gmail.com>
2021-07-05 03:49:51 -0700

    post to forum and toggle render post content

--- src/module/forumName.js
+++ src/module/forumName.js
@@ -10,52 +10,66 @@ class ForumName extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            forum: {
-                post: []
-            }
+
         }
+
+        this.handlePostRender = this.handlePostRender.bind(this);
         this.handlePost = this.handlePost.bind(this);
-        this.handleComment = this.handleComment.bind(this);
+        this.handleCommentRender = this.handleCommentRender.bind(this);
     }
 
     componentDidUpdate(prevProps, prevState) {
         const { params } = this.props.match;
+        if (params.name && prevProps.currentForum.name !== params.name) {
+            this.props.fetchForumByName(this.props.match.params.name);
+        }
+        if (this.props.currentForum.id !== prevProps.currentForum.id) {
 
-        if (params.name && prevState.forum.name !== params.name) {
-            this.getForum(params.name);
+            this.setState({
+                currentForum: {
+                    posts: this.props.currentForum.posts.map(item => ({ id: item.id, renderPost: false }))
+                }
+            });
 
         }
+
+        if (this.props.currentForum.newPost.id !== prevProps.currentForum.newPost.id) {
+
+            const posts = [...this.state.currentForum.posts];
+            posts.unshift({ id: this.props.currentForum.newPost.id, renderPost: false });
+            this.setState({ currentForum: { posts: posts } }, console.log(this.props));
+        }
+
     }
     componentDidMount() {
         this.props.fetchForumByName(this.props.match.params.name);
     }
-    getForum(name) {
-        axios.get(`${API_URL}/forum/${name}`)
-            .then(response => this.setState({ forum: response.data[0] }))
-            .catch(error => console.log(error));
-    }
-    handleClick = (e) => {
+
+    handlePostRender = (e, index) => {
         //add a render: true to render comments in a using the index of the post 
         //https://stackoverflow.com/questions/29537299/react-how-to-update-state-item1-in-state-using-setstate
 
-        let posts = [...this.state.forum.post];
+        let posts = [...this.state.currentForum.posts];
 
         //the first time it runs will create a new rednerCommet attribute and flip the undefined attribute to true with !
         //afterwards it will just flip the boolean with !
-        let post = { ...posts[e.target.value], renderComment: !this.state.forum.post[e.target.value].renderComment };
 
-        posts[e.target.value] = post;
+
+        let post = { ...posts[index], renderPost: !this.state.currentForum.posts[index].renderPost };
+
+        posts[index] = post;
 
         //https://www.codegrepper.com/code-examples/delphi/how+to+update+a+single+attribute+in+object+in+setstate+in+react
         this.setState(prevState => (
             {
-                forum: {
-                    ...prevState.forum,
-                    post: posts
+                currentForum: {
+                    ...prevState.currentForum,
+                    posts: posts
                 }
             }));
-        console.log(this.state.forum.post);
+
     }
+
     handlePost(event) {
         event.preventDefault();
         const title = event.target.newPostTitle.value;
@@ -63,48 +77,46 @@ class ForumName extends Component {
         if (!title || !content) {
             alert("Please fill in a title and a content for the post.");
         } else {
-            axios
-                .post(`${API_URL}/posts/`, {
+
+            this.props.createPostByForumID(
+                {
                     forum_id: this.props.currentForum.id,
                     title: title,
-                    content: content,
-                })
-                .then(response => {
+                    content: content
+                }
+            );
 
-                    alert("Post created");
-                    this.componentDidMount();
-                })
-                .catch(error => console.log(error));
+
         }
     }
-    handleComment(event) {
+
+    handleCommentRender(event) {
         event.preventDefault();
         const comment = event.target.comment.value;
         if (!comment) {
             alert("Please dont enter an empty comment.");
         } else {
-            axios
-                .post(`${API_URL}/forum/${this.props.match.params.name}/${event.target.id}`, {
-                    author: this.props.user.displayName,
-                    content: comment,
-                })
-                .then(response => {
-                    this.componentDidMount();
-                    // let updatePost = { ...this.state.forum.post }
+            // axios
+            //     .post(`${API_URL}/forum/${this.props.match.params.name}/${event.target.id}`, {
+            //         author: this.props.user.displayName,
+            //         content: comment,
+            //     })
+            //     .then(response => {
+            //         let updatePost = { ...this.state.forum.post }
 
-                    // updatePost[event.target.attributes.index.value].comment = response.data;
-                    // console.log(updatePost);
-                    // console.log(this.state);
-                    // this.setState(prevState => (
-                    //     {
-                    //         forum: {
-                    //             ...prevState.forum,
-                    //             post: { ...prevState.forum.post, comment: response.data }
-                    //         }
-                    //     }
-                    // ))
-                })
-                .catch(error => console.log(error));
+            //         updatePost[event.target.attributes.index.value].comment = response.data;
+            //         console.log(updatePost);
+            //         console.log(this.state);
+            //         this.setState(prevState => (
+            //             {
+            //                 forum: {
+            //                     ...prevState.forum,
+            //                     post: { ...prevState.forum.post, comment: response.data }
+            //                 }
+            //             }
+            //         ))
+            //     })
+            //     .catch(error => console.log(error));
 
         }
 
@@ -112,10 +124,9 @@ class ForumName extends Component {
     render() {
 
 
-
-        if (this.props.currentForum) {
-
+        if (this.props.currentForum && this.state.currentForum) {
             const { name, description, updated_at, posts } = this.props.currentForum;
+
             return (
                 <main className="forum-name">
                     <h1>{name}</h1>
@@ -131,16 +142,15 @@ class ForumName extends Component {
 
                     </form>
                     <section>
-                        {posts && sortByDate(posts).map((item, index) =>
-                            <div className="forum-name__post" key={item.id}>
+                        {posts && posts.map((item, index) =>
+                            <div className="forum-name__post" key={item.id} onClick={e => this.handlePostRender(e, index)}>
                                 <h2>{item.title}</h2>
-                                <p>Posted {timeDisplay(item.updated_at)}</p>
-                                {item.display &&
-                                    <div>
-
-                                    </div>
+                                <p className="forum-name__post__time-stamp">Posted {timeDisplay(item.updated_at)}</p>
+                                {this.state.currentForum.posts[index] && this.state.currentForum.posts[index].renderPost &&
+                                    <p className="forum-name__post__content">{item.content}</p>
                                 }
-                                {/* <button className="forum-name__post__btn--comment" onClick={this.handleClick} value={index}>{item.comment.length} Comments</button>
+                                <button className="forum-name__post__btn--comment" onClick={this.handleCommentRender} > Comments</button>
+                                {/* 
                                 {this.state.forum.post[index].renderComment &&
                                     <form className="forum-name__post__comment" onSubmit={this.handleComment} id={item.title} index={index}>
                                         <textarea className='forum-name__post__comment__textarea' id="comment" type='texarea' placeholder="respect others!" />
@@ -164,6 +174,7 @@ class ForumName extends Component {
 ForumName.propTypes = {
     fetchForumByName: PropTypes.func.isRequired,
     currentForum: PropTypes.object.isRequired,
+    createPostByForumID: PropTypes.func.isRequired
 }
 
 const mapStateToProps = (state) => {
@@ -173,6 +184,4 @@ const mapStateToProps = (state) => {
         currentForum: state.forums.currentForum
     };
 };
-export default connect(mapStateToProps, { fetchForumByName })(ForumName);
-
-const sortByDate = (arr) => arr.sort((a, b) => b.timestamp - a.timestamp);
+export default connect(mapStateToProps, { fetchForumByName, createPostByForumID })(ForumName);
